90a9ed79f53b51294557b068c648c6f18018be68
Alen <alen.rajher@gmail.com>
2025-11-14 08:52:50 +0100

    Add manual migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -2,6 +2,8 @@ import * as migration_20251107_183848_initial from './20251107_183848_initial';
 import * as migration_20251113_133855 from './20251113_133855';
 import * as migration_20251114_065142 from './20251114_065142';
 import * as migration_20251114_074047 from './20251114_074047';
+import * as migration_20251114_075131 from './20251114_075131';
+import * as migration_20251114_075232 from './20251114_075232';
 
 export const migrations = [
   {
@@ -22,6 +24,16 @@ export const migrations = [
   {
     up: migration_20251114_074047.up,
     down: migration_20251114_074047.down,
-    name: '20251114_074047'
+    name: '20251114_074047',
+  },
+  {
+    up: migration_20251114_075131.up,
+    down: migration_20251114_075131.down,
+    name: '20251114_075131',
+  },
+  {
+    up: migration_20251114_075232.up,
+    down: migration_20251114_075232.down,
+    name: '20251114_075232'
   },
 ];
